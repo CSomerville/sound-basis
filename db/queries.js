c551e2fe65746c9923293fbb4d6746f34d0b26cf
Colby Somerville <colby.somerville@gmail.com>
2016-07-17 15:05:04 -0400

    adds queries.pageUpdate

--- db/queries.js
+++ db/queries.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const connection = require('./connection');
 
 const queries = {};
@@ -20,3 +21,47 @@ queries.pageAllActive = () =>
       t.any(`SELECT * FROM items WHERE active = true`)
     ])
   );
+
+queries.pageCreate = newPage => {
+  return connection.db.task(t =>
+    t.none(`
+      INSERT INTO pages
+      (id, name, active, has_sub_pages, position)
+      VALUES ($1, $2, $3, $4, $5);
+    `, [newPage.id, newPage.name, newPage.active, newPage.hasSubPages, newPage.position])
+  );
+}
+
+queries.pageUpdate = (id, toUpdate) => {
+  const whiteListed = {
+    name: 'string',
+    has_sub_pages: 'boolean',
+    active: 'boolean',
+    position: 'number'
+  };
+
+  const keys = [];
+  const values = [];
+
+  for (key in toUpdate) {
+    assert(key in whiteListed,
+      `queries.pageUpdate does not have ${key} in whiteListed`);
+    assert.equal(typeof toUpdate[key], whiteListed[key],
+      `${key} must be ${whiteListed[key]}`);
+
+    keys.push(`${key}=$${keys.length + 1}`);
+    values.push(toUpdate[key]);
+  }
+
+  values.push(id);
+
+  const str = `
+    UPDATE pages SET ${keys.join(', ')}
+    , updated_at = NOW()
+    WHERE id = $${keys.length + 1};
+  `;
+
+  return connection.db.task(t => 
+    t.none(str, values)
+  )
+}
--- test/db/queries.spec.js
+++ test/db/queries.spec.js
@@ -1,3 +1,4 @@
+const uuid = require('node-uuid');
 const expect = require('chai').expect;
 const connection = require('../../db/connection');
 const schema = require('../../db/schema');
@@ -45,6 +46,75 @@ describe('queries', () => {
           done();
         })
         .catch(err => done(err)); 
+    });
+  });
+
+  describe('pageCreate', () => {
+    it('should add row to page table', done => {
+      
+      const newPage = {
+        id: uuid.v4(),
+        name: 'Oak',
+        hasSubPages: true,
+        active: false,
+        position: 2
+      };
+
+      queries.pageCreate(newPage)
+        .then(() =>
+          connection.db.task(t =>
+            t.one(`SELECT * FROM pages WHERE id = $1`, newPage.id)
+          )
+        )
+        .then(data => {
+          expect(data.id).to.equal(newPage.id);
+          expect(data.name).to.equal(newPage.name);
+          expect(data.has_sub_pages).to.equal(newPage.hasSubPages)
+          expect(data.active).to.equal(newPage.active);
+          expect(data.position).to.equal(newPage.position);
+          expect(data.created_at).to.exist;
+          expect(data.updated_at).to.be.null;
+          done();
+        })
+        .catch(err => done(err));
+    });
+
+    it('should throw without good input', done => {
+
+      const badInput = {};
+
+      queries.pageCreate(badInput)
+        .then(() => {
+          expect('in here').to.be.null;
+          done();
+        })
+        .catch(err => {
+          expect(err.message).to.equal('null value in column "id" violates not-null constraint');
+          done();
+        });
+    });
+  });
+  describe('pageUpdate', () => {
+    it('should use key value pairs to update columns', done => {
+      const seedId = 'cec16e9c-db72-407a-926c-7901e0ab5c8e'
+      const toUpdate = {
+        name: 'Laurel Oak',
+        active: true,
+      };
+      
+      queries.pageUpdate(seedId, toUpdate)
+        .then(() =>
+          connection.db.task(t =>
+            t.one(`SELECT * FROM pages WHERE id = $1`, seedId)
+          )
+        )
+        .then(data => {
+          expect(data.name).to.equal(toUpdate.name);
+          expect(data.active).to.equal(toUpdate.active);
+          expect(data.updated_at).to.exist;
+          done();
+        })
+        .catch(err => done(err));
     });
   });
 });
